411658e621a3bcff9020eabe9f80b2d1923049df
 vkorey <vvkorey@gmail.com>
2021-09-28 13:00:32 +0300

    test

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -11,9 +11,9 @@ class IngredientAdmin(admin.ModelAdmin):
         'name',
         'measurement_unit',
     )
-    list_filter = ('name',)
     empty_value_display = '-empty-'
-
+    list_filter = ('name',)
+    
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
