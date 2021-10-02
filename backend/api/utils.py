7f287f6309b65c2e4eac0fb889cd3a6f61e6360a
 vkorey <vvkorey@gmail.com>
2021-10-02 18:58:24 +0300

    review 1

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -6,8 +6,8 @@ from .models import IngredientRecipe
 def get_ingredients_list(recipes_list):
     ingredients_dict = {}
     ingredients = IngredientRecipe.objects.filter(
-        recpe__shoppingcart__user=recipes_list.get('user')
-    )
+        recpe__shoppingcart__user=recipes_list.user).values_list(
+                'ingredients__name', 'amount', 'ingredients__measurement_unit')
     for ingredient in ingredients:
         amount = ingredient.amount
         name = ingredient.ingredient.name
